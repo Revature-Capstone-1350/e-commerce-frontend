ca5e9d039d34bc25920775ff0788b719da57d40e
treymeetsworld <trevorzac16@yahoo.com>
2022-07-14 16:41:00 -0400

    changed store to use user state

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import orderReducer from './orderSlice'
+import userReducer from './userSlice'
 
 export const store = configureStore({
   reducer: {
-    orders: orderReducer
+    user: userReducer
   },
 })
 
---
+++ src/store/userSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+
+export interface UserState {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  role_id: number;
+}
+
+const initialState: UserState = {
+  id: 0,
+  firstName: '',
+  lastName: '',
+  email: '',
+  password: '',
+  role_id: 0
+}
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    
+  }
+});
+
+export const {  } = userSlice.actions
+
+export default userSlice.reducer
